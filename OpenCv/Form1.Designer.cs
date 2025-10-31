f05f7d36989181edffb163d312a0ece8beeecdb8
omerserrdar <omerserrdar@gmail.com>
2025-10-31 14:37:25 +0300

    Proje dosyası ekle.

---
+++ OpenCv/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace YeniSansurProjesi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnResimAc = new System.Windows.Forms.Button();
+            this.btnSansurle = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnResimAc
+            // 
+            this.btnResimAc.Location = new System.Drawing.Point(112, 354);
+            this.btnResimAc.Name = "btnResimAc";
+            this.btnResimAc.Size = new System.Drawing.Size(190, 57);
+            this.btnResimAc.TabIndex = 0;
+            this.btnResimAc.Text = "Resim Aç";
+            this.btnResimAc.UseVisualStyleBackColor = true;
+            this.btnResimAc.Click += new System.EventHandler(this.btnResimAc_Click_1);
+            // 
+            // btnSansurle
+            // 
+            this.btnSansurle.Location = new System.Drawing.Point(369, 354);
+            this.btnSansurle.Name = "btnSansurle";
+            this.btnSansurle.Size = new System.Drawing.Size(172, 57);
+            this.btnSansurle.TabIndex = 1;
+            this.btnSansurle.Text = "Yüzleri Sansürle";
+            this.btnSansurle.UseVisualStyleBackColor = true;
+            this.btnSansurle.Click += new System.EventHandler(this.btnSansurle_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(112, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(429, 291);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 462);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSansurle);
+            this.Controls.Add(this.btnResimAc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnResimAc;
+        private System.Windows.Forms.Button btnSansurle;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private EventHandler Form1_Load;
+
+        public Form1(EventHandler form1_Load)
+        {
+            Form1_Load = form1_Load;
+        }
+    }
+}
+
+
---
+++ OpenCv/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+
+// Projenizin adı ne olursa olsun, bu kod çalışacaktır.
+namespace YeniSansurProjesi // (Burası sizde "WindowsFormsApp1" vb. olabilir, sorun değil)
+{
+    public partial class Form1 : Form
+    {
+        private CascadeClassifier _faceCascade;
+        private Image<Bgr, byte> _originalImage;
+
+        public Form1()
+        {
+            InitializeComponent(); // Burası Tasarım ekranını (Adım 4) koda bağlar
+            try
+            {
+                // Burası Model dosyasını (Adım 3) yükler
+                _faceCascade = new CascadeClassifier("haarcascade_frontalface_default.xml");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("HATA: 'haarcascade_frontalface_default.xml' dosyası 'bin/Debug' klasöründe bulunamadı.\n" + ex.Message);
+            }
+        }
+
+        // "Resim Aç" butonu için kod
+        private void btnResimAc_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Resim Dosyaları (*.jpg; *.png; *.bmp)|*.jpg; *.png; *.bmp";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    _originalImage = new Image<Bgr, byte>(ofd.FileName);
+                    pictureBox1.Image = _originalImage.ToBitmap();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Resim yüklenirken hata oluştu: " + ex.Message);
+                }
+            }
+        }
+
+        // "Yüzleri Sansürle" butonu için kod
+        private void btnSansurle_Click(object sender, EventArgs e)
+        {
+            if (_originalImage == null)
+            {
+                MessageBox.Show("Lütfen önce bir resim açın!");
+                return;
+            }
+            try
+            {
+                Image<Bgr, byte> censoredImage = _originalImage.Clone();
+                Image<Gray, byte> grayImage = censoredImage.Convert<Gray, byte>();
+
+                Rectangle[] faces = _faceCascade.DetectMultiScale(grayImage, 1.1, 5, new Size(20, 20));
+
+                if (faces.Length == 0)
+                {
+                    MessageBox.Show("Resimde hiç yüz bulunamadı.");
+                    return;
+                }
+                foreach (Rectangle face in faces)
+                {
+                    ApplyMosaic(censoredImage, face, 15); // 15: Mozaik boyutu
+                }
+                pictureBox1.Image = censoredImage.ToBitmap();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Sansürleme sırasında hata: " + ex.Message);
+            }
+        }
+
+        // Mozaikleme fonksiyonu
+        private void ApplyMosaic(Image<Bgr, byte> image, Rectangle area, int pixelSize)
+        {
+            if (pixelSize <= 0) pixelSize = 1;
+            if (area.Width <= 0 || area.Height <= 0) return;
+            Image<Bgr, byte> faceArea = image.GetSubRect(area);
+            Size smallSize = new Size(faceArea.Width / pixelSize, faceArea.Height / pixelSize);
+            if (smallSize.Width == 0) smallSize.Width = 1;
+            if (smallSize.Height == 0) smallSize.Height = 1;
+            Image<Bgr, byte> smallFace = new Image<Bgr, byte>(smallSize);
+            CvInvoke.Resize(faceArea, smallFace, smallSize, 0, 0, Inter.Nearest);
+            CvInvoke.Resize(smallFace, faceArea, area.Size, 0, 0, Inter.Nearest);
+        }
+
+        private void btnResimAc_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSansurle_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
